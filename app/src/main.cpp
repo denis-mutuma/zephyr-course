06de05462349eac913f91acec66ec0f150b31aa5
denis-mutuma <mutuma212@gmail.com>
2026-05-06 20:16:07 +0300

    feat: add configurable heartbeat LED

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,26 +3,26 @@
 #include <zephyr/logging/log.h>
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+#define HEARTBEAT_LED DT_ALIAS(app_led)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct gpio_dt_spec heartbeat_led = GPIO_DT_SPEC_GET(HEARTBEAT_LED, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    bool led_state = true;
+    bool heartbeat_led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&heartbeat_led)) return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&heartbeat_led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (gpio_pin_toggle_dt(&heartbeat_led) < 0) return 0;
 
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        heartbeat_led_state = !heartbeat_led_state;
+        LOG_INF("Heartbeat LED state: %s", heartbeat_led_state ? "ON" : "OFF");
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
