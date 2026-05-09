70b0604cf7303dcedf99e614f8a878fbe423856d
denis-mutuma <mutuma212@gmail.com>
2026-05-09 18:13:59 +0300

    feat: add sensor driver implementing the sensor_sample_fetch and sensor_channel_get APIs

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,60 @@
+#include "zephyr/devicetree.h"
+#include <stdint.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+#define DT_DRV_COMPAT our_driver
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+struct our_driver_data {
+    int32_t sample_celsius;
+    uint32_t fetch_count;
+};
+
+static int sample_fetch_my_impl(const struct device *dev, enum sensor_channel chan) {
+    struct our_driver_data *data = dev->data;
+
+    if(chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_AMBIENT_TEMP) {
+        return -ENOTSUP;
+    }
+
+    data->fetch_count++;
+    data->sample_celsius = 20 + data->fetch_count;
+
+    LOG_INF("Fetched sample: %d C", data->sample_celsius);
+    return 0;
+}
+
+static int channel_get_my_impl(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
+
+    struct our_driver_data *data = dev->data;
+
+    if(chan != SENSOR_CHAN_AMBIENT_TEMP) {
+        return -ENOTSUP;
+    }
+
+    val->val1 = data->sample_celsius;
+    val->val2 = 0;
+
+    LOG_INF("Channel get: %d.%06d C", val->val1, val->val2);    
+    return 0;
+}
+
+static DEVICE_API(sensor, api_denis) = {
+    .sample_fetch = sample_fetch_my_impl,
+    .channel_get = channel_get_my_impl,
+};
+
+
+// init fn
+static int init(const struct device *dev) {
+    LOG_INF("Device initialized!");
+    return 0;
+}
+
+#define DEV_INST(inst) \
+    static struct our_driver_data our_driver_data_##inst; \
+    DEVICE_DT_INST_DEFINE(inst, init, NULL, &our_driver_data_##inst, NULL, POST_KERNEL, 80, &api_denis);
+
+DT_INST_FOREACH_STATUS_OKAY(DEV_INST)
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,3 +1,5 @@
+#include "zephyr/device.h"
+#include "zephyr/drivers/sensor.h"
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -8,6 +10,24 @@
 static const struct gpio_dt_spec heartbeat_led = GPIO_DT_SPEC_GET(HEARTBEAT_LED, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+namespace {
+    void test() {
+        const struct device* driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+        struct sensor_value val;
+
+        if(!device_is_ready(driver)) {
+            LOG_ERR("our_driver0 is not ready");
+            return;
+        }
+
+        auto ret = sensor_sample_fetch(driver);
+        LOG_INF("Fetch ret %d", ret);
+        
+        ret = sensor_channel_get(driver, SENSOR_CHAN_AMBIENT_TEMP, &val);
+        LOG_INF("Channel ret %d, temp %d.%06d C", ret, val.val1, val.val2);
+    }
+}
 
 int main(void)
 {
